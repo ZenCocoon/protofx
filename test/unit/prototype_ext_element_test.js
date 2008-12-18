23d57519424417fc7e072f45ac80995f7cce4359
ZenCocoon <public@zencocoon.com>
2008-12-18 10:11:27 +0100

    Makes testing more reliable by reinitializing the sandbox elements before each test

--- test/unit/prototype_ext_element_test.js
+++ test/unit/prototype_ext_element_test.js
@@ -1,36 +1,100 @@
 new Test.Unit.Runner({
+  setup: function(){
+    // Would be good to load fixture instead
+    $('sandbox').insert('<div id="div"> </div>');
+  },
+  
+  teardown: function(){
+    $('div').remove();
+  },
+  
   testFade: function(){
     Object.extend(FX.DefaultOptions, {duration: 500});
+    var element = $('div');
     
-    $('div').fade();
+    element.fade();
     this.wait(550, function(){
-      this.assertEqual(0, $('div').getOpacity());
-    });
-    
-    $('div').show().fade({duration: 1000});
-    this.wait(550, function(){
-      this.assertNotEqual(0, $('div').getOpacity());
-      this.wait(500, function(){
-        this.assertEqual(0, $('div').getOpacity());
+      this.assertEqual(0, element.getOpacity());
+      
+      element.setOpacity(1);
+      this.assertEqual(1, element.getOpacity());
+      element.fade({duration: 1000});
+      this.wait(550, function(){
+        this.assertNotEqual(0, element.getOpacity());
+        this.wait(500, function(){
+          this.assertEqual(0, element.getOpacity());
+        });
       });
     });
   },
   
   testAppear: function(){
     Object.extend(FX.DefaultOptions, {duration: 500});
-    $('div').hide();
-    this.assertEqual(0, $('div').getOpacity());
+    var element = $('div');
     
-    $('div').appear();
+    element.hide(0);
+    this.assertNotVisible(element);
+    
+    element.appear();
     this.wait(550, function(){
-      this.assertEqual(1, $('div').getOpacity());
+      this.assertEqual(1, element.getOpacity());
+      
+      element.setOpacity(0)
+      this.assertEqual(0, element.getOpacity());
+      element.appear({duration: 1000});
+      this.wait(550, function(){
+        this.assertNotEqual(1, element.getOpacity());
+        this.wait(500, function(){
+          this.assertEqual(1, element.getOpacity());
+        });
+      });
     });
+  },
+  
+  testBlindUp: function(){
+    Object.extend(FX.DefaultOptions, {duration: 500});
+    var element = $('div');
+    element.setStyle({height: '200px'});
     
-    $('div').hide().appear({duration: 1000});
+    this.assertEqual(200, element.getHeight());
+    element.blindUp();
+    // TODO: Why to restore to original height and keep it hidden ? Why not keep it to height: 0 ?
     this.wait(550, function(){
-      this.assertNotEqual(1, $('div').getOpacity());
-      this.wait(500, function(){
-        this.assertEqual(1, $('div').getOpacity());
+      this.assertEqual(0, element.getHeight());
+    
+      element.setStyle({height: '200px'}).blindUp({duration: 1000});
+      this.wait(550, function(){
+        this.assertNotEqual(200, element.getHeight());
+        this.assertNotEqual(0, element.getHeight());
+        this.wait(500, function(){
+          this.assertEqual(0, element.getHeight());
+        });
+      });
+    });
+  },
+  
+  testBlindDown: function(){
+    Object.extend(FX.DefaultOptions, {duration: 500});
+    var element = $('div');
+    element.setStyle({height: '200px'}).hide();
+    
+    this.assertEqual(200, element.getHeight());
+    this.assertNotVisible(element);
+    element.blindDown();
+    this.wait(550, function(){
+      this.assertEqual(200, element.getHeight());
+    
+      element.hide();
+      this.assertEqual(200, element.getHeight());
+      this.assertNotVisible(element);
+      element.blindDown({duration: 1000});
+      this.wait(550, function(){
+        this.assertVisible(element);
+        this.assertNotEqual(200, element.getHeight());
+        this.assertNotEqual(0, element.getHeight());
+        this.wait(500, function(){
+          this.assertEqual(200, element.getHeight());
+        });
       });
     });
   }
